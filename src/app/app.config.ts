d6928eaefd71993618eaa4f8f2b992ce6a986e50
fmoctavioe-ux <fmoctavioe@unincca.edu.coi>
2026-02-28 11:29:08 -0500

    Agregando provideHttpClient para que funcione en Vercel

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-// 1. Importamos la línea telefónica (HttpClient)
+// ESTA ES LA LÍNEA MÁGICA QUE FALTA:
 import { provideHttpClient } from '@angular/common/http'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    // 2. Le conectamos la línea a nuestra aplicación
+    // AQUÍ LE DAMOS EL PERMISO PARA USAR INTERNET:
     provideHttpClient() 
   ]
 };
